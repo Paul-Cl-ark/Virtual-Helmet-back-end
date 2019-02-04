5556582c4cf37077f4d19324d797f26192ff7c30
Paul-Cl-ark <clark.paul123@gmail.com>
2019-02-04 13:27:02 +0000

    refactors to use arrow functions and return user on successful fetch

--- app/api/controllers/users.js
+++ app/api/controllers/users.js
@@ -1,24 +1,20 @@
 const userModel = require('../models/users')
-const bcrypt = require('bcryptjs')
 const passport = require('passport')
 
 // passport config
 require('../../../config/passport')(passport)
 
-const jwt = require('jsonwebtoken')
 module.exports = {
-	create: function(req, res, next) {
-		userModel.create(
-			{
-				firstName: req.body.firstName,
-				lastName: req.body.lastName,
-				email: req.body.email,
-				password: req.body.password
-			},
-			function(err, result) {
-				if (err) next(err)
-				else res.json({ status: 'success', message: 'User added successfully!', data: null })
-			}
-		)
+	create: (req, res, next) => {
+		newUser = {
+			firstName: req.body.firstName,
+			lastName: req.body.lastName,
+			email: req.body.email,
+			password: req.body.password
+		}
+		userModel.create(newUser, (err, result) => {
+			if (err) next(err)
+			else res.json({ status: 'success', message: 'User added successfully!', data: newUser })
+		})
 	}
 }

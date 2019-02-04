3f9db40e09a70a93fd7a21bb6c73cd83f76b5187
Paul-Cl-ark <clark.paul123@gmail.com>
2019-02-04 13:37:25 +0000

    refactors to use arrow functions

--- routes/users.js
+++ routes/users.js
@@ -9,15 +9,15 @@ require('../config/passport')(passport)
 
 router.post('/register', userController.create)
 
-router.post('/authenticate', function(req, res, next) {
-	passport.authenticate('local', function(err, user, info) {
+router.post('/authenticate', (req, res, next) => {
+	passport.authenticate('local', (err, user, info) => {
 		if (err) {
 			return next(err)
 		}
 		if (!user) {
 			return res.json({ message: 'Invalid credentials' })
 		}
-		req.logIn(user, function(err) {
+		req.logIn(user, err => {
 			if (err) {
 				return next(err)
 			}

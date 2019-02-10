dc04af12050f796d519fd30279c57310e3c1bd3f
Paul-Cl-ark <clark.paul123@gmail.com>
2019-02-10 12:30:13 +0000

    adds auth back in

--- routes/spots.js
+++ routes/spots.js
@@ -4,9 +4,9 @@ const spotController = require('../app/api/controllers/spots')
 const { ensureAuthenticated } = require('../helpers/auth')
 
 router.get('/', spotController.getAll)
-router.post('/', spotController.create)
+router.post('/', ensureAuthenticated, spotController.create)
 router.get('/user-spots', ensureAuthenticated, spotController.getUserSpotsOnly)
 router.get('/:spotId', spotController.getById)
-router.put('/:spotId', spotController.updateById)
-router.delete('/:spotId', spotController.deleteById)
+router.put('/:spotId', ensureAuthenticated, spotController.updateById)
+router.delete('/:spotId', ensureAuthenticated, spotController.deleteById)
 module.exports = router
